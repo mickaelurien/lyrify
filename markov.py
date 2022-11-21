070c723f17457b1ee2abe8012ddd23e49c5efb31
Mickaël <mickael.urien1@gmail.com>
2022-11-21 10:37:02 +0100

    [+] Scrape protection added - dynamic headers & ip address

--- markov.py
+++ markov.py
@@ -1,56 +1,93 @@
 import json
 import random 
 import re
+import yaml
 import sys
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from collections import defaultdict, Counter
 
-# FIXME This is not working yet, websites actually detect i'm a robot 🤖
-# Take an url in parameter and return the lyrics of the artist
-def scrape_text(domain = 'https://www.azlyrics.com', artist = 'damso'): 
+with open('headers.yml') as f_headers:
+    browser_headers = yaml.safe_load(f_headers)
+
+with open('user_agents.yml') as f_user_agents:
+    user_agents = yaml.safe_load(f_user_agents)
+
+# Scrape a list of proxies from https://free-proxy-list.net/
+def scrape_proxies(): 
+    response = requests.get('https://free-proxy-list.net/')
+    proxy_list = pd.read_html(response.text)[0]
+    proxy_list['url'] = 'http://' + proxy_list['IP Address'] + ':' + proxy_list['Port'].astype(str)
+    proxy_list.head()
+    https_proxies = proxy_list[proxy_list['Https'] == "yes"] # Select only https proxies
+    return select_good_proxies(https_proxies)
+
+# Get a list of proxies in params and return a good proxies array, checked by httpbin.org/ip
+def select_good_proxies(proxies, nb_needed=3):
+    url = "https://httpbin.org/ip"
+    good_proxies = []
+    headers = browser_headers["Chrome"] # Using Chrome headers, no reason to use another one
+
+    for proxy_url in proxies["url"]: # For each proxy in the list
+        proxies = {
+            "http": proxy_url,
+            "https": proxy_url
+        }
+
+        try: # We make a request to httpbin.org/ip and see the response
+            response = requests.get(url, headers=headers, proxies=proxies, timeout=2)
+            good_proxies.append(proxy_url)
+            # print("Good proxy: {}".format(proxy_url))
+        except Exception:
+            pass
+            
+        if len(good_proxies) >= nb_needed:
+            break
+    # print("Good proxies: {}\n".format(good_proxies))
+    return good_proxies
+    
+
+# Scrape the artist page to get all the songs
+# Then use our proxy/headers protection to scrappe all lyrics from the songs and save them in jsons/*artist_name*.json
+def scrape_text(domain = 'https://www.azlyrics.com', artist = 'damso'):
+    good_proxies = scrape_proxies() 
+        
     artist_url = '/' + artist[0] + '/' + artist + '.html' # if artist is damso, the url will be /d/damso.html
     url = domain + artist_url
-    print(url)
-    response = requests.get(url)
-    text = []
+    response = requests.get(url) # The first request to access the artist page is not under protection
     if response.ok: 
         soup = BeautifulSoup(response.text, 'html.parser')
         items = soup.find_all('div', class_='listalbum-item')
-        links = [domain+item.find('a').get('href') for item in items]
-        print(links)
-        nb_links = len(links) if len(links) < 30 else 30
+        links = [domain+item.find('a').get('href') for item in items] # Get all the links to the songs
+        # print(links)
+        nb_links = len(links) if len(links) < 30 else 30 # We don't want to scrappe too much songs, so we limit to 30
         cnt = 0
-        for link in links:
-            if cnt < 1: 
-                cnt += 1
+        lyrics = ''
+        for link in links: # For each song
+            if cnt < nb_links: 
                 print('scraping link {}/{}'.format(cnt, nb_links))
-                response = requests.get(link)
-                if response.ok:
+                browser, headers = random.choice(list(browser_headers.items())) # Get a random header from headers.yml
+                print(f"\n Using {browser} headers \n")
+                proxy_url = random.choice(good_proxies) # Get a random proxy from the good proxies list
+                proxies = proxies = {
+                    "http": proxy_url,
+                    "https": proxy_url
+                }
+                try: 
+                    response = requests.get(link, headers=headers, proxies=proxies, timeout=2)
                     soup = BeautifulSoup(response.text, 'html.parser')
-                    lyrics_parent = soup.find('div', class_='ringtone').find_next_sibling('div')
-                    test = [item.text for item in lyrics_parent]
-                    lyrics = []
-                    print(test)
-                    # for lyrics_node in lyrics_parent:
-                    #     textParsed = re.sub("[\(\[].*?[\)\]]", "", lyrics_node.text)
-                    #     lyrics.append(textParsed)
-                    #     print(lyrics_node)
-                    # # print(lyrics)
-
-                    # jsonContent = json.dumps(lyrics)
-                    # with open('./jsons/damso.json', 'w') as f:
-                    #     f.write(jsonContent)
-                    #     json.dump(jsonContent, f, indent=4)
-                    # text.append(lyrics)
+                    main_div = soup.find('div', class_='ringtone').find_next_sibling('div') # Target the div with the lyrics
+                    lyrics += main_div.text
+                    cnt += 1 # Increment the counter if all went well
+                    print(lyrics)  
+                except Exception:
+                    print(f"Proxy {proxy_url} failed")
         
-        return text
-
-result = scrape_text()
-print(result)
+        with open(f'jsons/{artist}.json', 'w') as json_file: # Save the lyrics in jsons/*artist_name*.json
+            json.dump(lyrics, json_file) 
 
 # Build the markov model, return a dictionary with the state as key and the next state with the probability as value
-
 # e.g : { "hello" : {"w" : 1} }
 # that means the world "hello" is followed by "w" 1 time (maybe because the text was 'hello world')
 def build_markov_model(train_text, state_size=1):
@@ -62,7 +99,7 @@ def build_markov_model(train_text, state_size=1):
     return markov_model
 
 # Generate the word from the markov model
-def generate_text(markov_model, speech_size=300):
+def generate_text(markov_model, speech_size=600):
     all_done = False
     speech = []
 
@@ -88,9 +125,11 @@ def generate_text(markov_model, speech_size=300):
 
 
 
-# damso_text = open('texts/damso.txt', 'r').read()
-# model = build_markov_model(damso_text, state_size=5)
+
+scrape_text()
+damso_text = json.load(open('jsons/damso.json'))
+model = build_markov_model(damso_text, state_size=8)
 
 # json.dump(model, open("./jsons/model.json", "w"))
-
-# generate_text(model)
+print('-------------------------------------------------------')
+generate_text(model)
