d53d2bf7368060946f08b0fed1b27d5c6b79903f
Mickaël <mickael.urien1@gmail.com>
2022-11-25 21:19:48 +0100

    [~] Update README.MD

--- back/markov.py
+++ back/markov.py
@@ -1,34 +1,9 @@
 import json
 import random
-import re
-import sys
-import requests
+# import re
+# import sys
+# import requests
 import markovify
-
-# Generate the word from the markov model
-def generate_text(markov_model, speech_size=600):
-    all_done = False
-    speech = []
-
-    while not all_done:
-        this_state = random.choice(list(markov_model))
-        speech += list(this_state)
-        
-        for i in range(speech_size):
-            population = list(markov_model[this_state])
-            weight = markov_model[this_state].values()
-            if len(population) == 0:
-                break
-
-            next_state = random.choices(population, weight)[0]
-            speech.extend(next_state)
-
-            if (len(speech) >= speech_size):
-                all_done = True
-                break
-
-            this_state = this_state[:1] + speech[-1]
-    print(''.join(speech))
 
 def generate_lyrics(artist):
     with open(f'./texts/{artist}.txt', 'r', encoding='utf-8') as f:
